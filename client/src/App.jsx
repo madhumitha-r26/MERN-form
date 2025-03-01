cf29261f4b7530f804de15d5f631c4da958d4eeb
madhumitha-ravichandran26 <madhumitha26301@gmail.com>
2025-03-01 11:32:58 +0530

    completed crud

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Login from "./Components/Login";
+import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
 import Welcome from "./components/Welcome";
